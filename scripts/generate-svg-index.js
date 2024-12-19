ff6f1912ac0a182052a218fabc7a3cf175fd2905
hyuni106 <suhyuni106@hanmail.net>
2024-12-19 15:52:21 +0900

    add svg index generator scripts

---
+++ scripts/generate-svg-index.js
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require('fs');
+const camelCase = require('camelcase');
+
+let contents = '// This file was created by the generate-svg-index script.\n';
+let exportObj = 'export {\n';
+
+const REGEX_SVG = /\.svg$/;
+
+const fileNames = fs.readdirSync('./src/assets/svgs/').sort();
+fileNames.forEach(fileName => {
+  if (REGEX_SVG.test(fileName)) {
+    const name = fileName.replace(REGEX_SVG, '');
+    const camelCasedName = camelCase(name, { pascalCase: true });
+    contents += `import ${camelCasedName} from 'assets/svgs/${fileName}';\n`;
+    exportObj += `  ${camelCasedName},\n`;
+  }
+});
+
+exportObj += '};';
+contents += `\n${exportObj}\n`;
+
+fs.writeFileSync('./src/assets/svgs/index.ts', contents);
--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,5 +1,8 @@
-import ImgBbangeRound from './img_bbange_round.svg';
+// This file was created by the generate-svg-index script.
+import IcHomeActive from 'assets/svgs/ic_home_active.svg';
+import IcHomeDefault from 'assets/svgs/ic_home_default.svg';
+import IcSettingActive from 'assets/svgs/ic_setting_active.svg';
+import IcSettingDefault from 'assets/svgs/ic_setting_default.svg';
+import ImgBbangeRound from 'assets/svgs/img_bbange_round.svg';
 
-export {
-  ImgBbangeRound
-}
+export { IcHomeActive, IcHomeDefault, IcSettingActive, IcSettingDefault, ImgBbangeRound };
