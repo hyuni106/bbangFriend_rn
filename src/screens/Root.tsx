fd69d8a6ae6351fc65dd316ac7c2baca43b5f4d2
hyuni106 <suhyuni106@hanmail.net>
2024-12-19 12:31:39 +0900

    fix Root.tsx

--- src/screens/Root.tsx
+++ src/screens/Root.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import Navigation from 'navigation/Navigation';
+import { StyleSheet } from 'react-native';
 
 const Root = (): React.ReactElement => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.safeAreaView}>
       <Navigation />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+  },
+});
 
 export default Root;
