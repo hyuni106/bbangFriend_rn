b1a49752d20d7257f403c11155e7270f432ca845
hyuni106 <suhyuni106@hanmail.net>
2024-12-20 16:53:46 +0900

    create default NavigationBar

---
+++ src/components/NavigationBar/NavBar.tsx
@@ -0,0 +1,87 @@
+import React, { useMemo } from 'react';
+import {
+  StyleProp,
+  ViewStyle,
+  StyleSheet,
+  View,
+  ColorValue,
+  useWindowDimensions,
+  ScaledSize,
+  Text,
+} from 'react-native';
+
+import { Colors, Dimens, Typography } from 'styles';
+import NavBarButton, { BUTTON_WIDTH, NavBarButtonProps } from './NavBarButton';
+
+const leftButtonMarginLeft = 11.5;
+const rightButtonMarginRight = 14;
+const titleMarginHorizontal = Math.max(leftButtonMarginLeft, rightButtonMarginRight) + BUTTON_WIDTH;
+
+export type Button = Omit<NavBarButtonProps, 'style' | 'hitSlop' | 'color'>;
+
+export interface NavBarProps {
+  style?: StyleProp<ViewStyle>;
+  backgroundColor?: ColorValue;
+  leftButton?: Button;
+  rightButton?: Button;
+  title?: string;
+  titleColor?: ColorValue;
+}
+
+const NavBar = (props: NavBarProps): React.ReactElement => {
+  const window = useWindowDimensions();
+  const styles = useMemo(() => createStyles(window), [window]);
+
+  const {
+    style,
+    backgroundColor = Colors.white,
+    leftButton,
+    rightButton,
+    title,
+    titleColor = Colors.gray1,
+  } = props;
+
+  return (
+    <View style={[styles.root, { backgroundColor }, style]}>
+      <View style={[styles.navBarContainer]}>
+        {leftButton && <NavBarButton style={styles.leftButton} {...leftButton} />}
+        <Text style={[styles.title, { color: titleColor }]} numberOfLines={1} lineBreakMode="tail">
+          {title}
+        </Text>
+        {rightButton && <NavBarButton style={styles.rightButton} {...rightButton} />}
+      </View>
+    </View>
+  );
+};
+
+const createStyles = (scaledSize: ScaledSize) => {
+  return StyleSheet.create({
+    root: {
+      width: Dimens.screenWidth,
+      height: Dimens.navBarHeight,
+    },
+    navBarContainer: {
+      flex: 1,
+      justifyContent: 'center',
+    },
+    leftButton: {
+      position: 'absolute',
+      top: 9,
+      left: leftButtonMarginLeft,
+    },
+    title: {
+      position: 'absolute',
+      ...Typography.title_18B,
+      textAlign: 'center',
+      left: titleMarginHorizontal,
+      width: scaledSize.width - titleMarginHorizontal * 2,
+    },
+    rightButton: {
+      position: 'absolute',
+      top: 9,
+      right: rightButtonMarginRight,
+    },
+  });
+};
+
+export default NavBar;
---
+++ src/components/NavigationBar/NavBarButton.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleProp, ViewStyle, StyleSheet } from 'react-native';
+import { SvgProps } from 'react-native-svg';
+
+import { TouchableOpacity } from 'components/Button';
+
+export const BUTTON_WIDTH = 35;
+
+export interface NavBarButtonProps {
+  style?: StyleProp<ViewStyle>;
+  icon: React.ComponentType<SvgProps>;
+  onPress?: () => void;
+}
+
+const NavBarButton = (props: NavBarButtonProps): React.ReactElement => {
+  const { style, onPress } = props;
+
+  return (
+    <TouchableOpacity style={[styles.root, style]} hitSlop={8} onPress={onPress}>
+      <props.icon width={BUTTON_WIDTH} height={BUTTON_WIDTH} />
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    width: BUTTON_WIDTH,
+    height: BUTTON_WIDTH,
+  },
+});
+
+export default NavBarButton;
