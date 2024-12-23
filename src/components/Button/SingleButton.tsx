d0e1512948dc8d9d2221ddfa578aef21b247137f
hyuni106 <suhyuni106@hanmail.net>
2024-12-23 16:04:27 +0900

    create SingleButton component

---
+++ src/components/Button/SingleButton.tsx
@@ -0,0 +1,72 @@
+import React, { useCallback } from 'react';
+import {
+  StyleProp,
+  ViewStyle,
+  Pressable,
+  PressableProps,
+  StyleSheet,
+  GestureResponderEvent,
+  Text,
+  ColorValue,
+} from 'react-native';
+
+import { Colors, Typography } from 'styles';
+import { preventDoubleTap } from 'utils/preventDoubleTap';
+
+interface SingleButtonProps extends Pick<PressableProps, 'disabled' | 'onPress'> {
+  style?: StyleProp<ViewStyle>;
+  title: string;
+  defaultColor?: ColorValue;
+  pressedColor?: ColorValue;
+}
+
+const SingleButton = (props: SingleButtonProps): React.ReactElement => {
+  const { style, title, defaultColor = Colors.gray4, pressedColor = Colors.gray3 } = props;
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const _onPress = useCallback(
+    preventDoubleTap((event: GestureResponderEvent): void => {
+      props.onPress?.(event);
+    }),
+    [props.onPress],
+  );
+
+  return (
+    <Pressable
+      {...props}
+      onPress={_onPress}
+      style={({ pressed }) => [
+        style,
+        styles.root,
+        {
+          backgroundColor: pressed ? pressedColor : defaultColor,
+        },
+      ]}>
+      <Text
+        numberOfLines={1}
+        lineBreakMode="tail"
+        style={[
+          styles.title,
+          {
+            color: Colors.white,
+          },
+        ]}>
+        {title}
+      </Text>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    height: 56,
+    paddingHorizontal: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+  },
+  title: {
+    ...Typography.title_16B,
+  },
+});
+
+export default SingleButton;
--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -2,5 +2,6 @@ import TouchableOpacity from './TouchableOpacity';
 import TouchableWithoutFeedback from './TouchableWithoutFeedback';
 import AddButton from './AddButton';
 import RemoveButton from './RemoveButton';
+import SingleButton from './SingleButton';
 
-export { TouchableOpacity, TouchableWithoutFeedback, AddButton, RemoveButton };
+export { TouchableOpacity, TouchableWithoutFeedback, AddButton, RemoveButton, SingleButton };
