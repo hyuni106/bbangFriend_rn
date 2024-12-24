31cc058ec90a7d1a4c3ca85ad70b4a20e992c508
hyuni106 <suhyuni106@hanmail.net>
2024-12-24 11:12:57 +0900

    create RatioCalculatorScreen

--- src/navigation/NavigationProps.ts
+++ src/navigation/NavigationProps.ts
@@ -11,6 +11,7 @@ export type StackParamList = {
   [ScreenName.CreateRecipeScreen]: undefined;
   [ScreenName.ModifyRecipeScreen]: undefined;
   [ScreenName.RecipeDetailScreen]: undefined;
+  [ScreenName.RatioCalculatorScreen]: undefined;
   [ScreenName.SettingScreen]: undefined;
 };
 
