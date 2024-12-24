4b570a2a633b9acd45280e3cc303fcf4935efd06
hyuni106 <suhyuni106@hanmail.net>
2024-12-24 13:41:57 +0900

    apply TabBar and TabView in RatioCalculatorScreen

--- src/screens/RatioCalculator/index.tsx
+++ src/screens/RatioCalculator/index.tsx
@@ -1,15 +1,49 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
+import { SceneRendererProps, NavigationState } from 'react-native-tab-view';
+import { useTranslation } from 'react-i18next';
 
 import { Props } from 'navigation/NavigationProps';
 import { ScreenName } from 'navigation/ScreenNames';
 import { BackButtonNavBar } from 'components/NavigationBar';
 import { Colors } from 'styles';
+import { TabBar } from 'components/common/Base';
+import TabView, { TabRoute } from 'components/common/Base/TabView';
 
 const RatioCalculatorScreen = ({}: Props<ScreenName.RatioCalculatorScreen>): React.ReactElement => {
+  const { t } = useTranslation();
+  const [index, setIndex] = React.useState<number>(0);
+
+  const routes: TabRoute[] = [
+    { key: 'material_based', title: t('calculator.tab.material_based') },
+    { key: 'ratio_based', title: t('calculator.tab.ratio_based') },
+  ];
+
+  const renderTabBar = (
+    props: SceneRendererProps & { navigationState: NavigationState<TabRoute> },
+  ) => <TabBar props={props} activeColor={Colors.blue1} />;
+
+  const renderScene = ({ route }: { route: TabRoute }) => {
+    switch (route.key) {
+      case 'material_based':
+        return <View />;
+      case 'ratio_based':
+        return <View />;
+      default:
+        return <></>;
+    }
+  };
+
   return (
     <View style={styles.root}>
-      <BackButtonNavBar />
+      <BackButtonNavBar title={t('calculator.title')} />
+      <TabView
+        index={index}
+        routes={routes}
+        renderTabBar={renderTabBar}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+      />
     </View>
   );
 };
