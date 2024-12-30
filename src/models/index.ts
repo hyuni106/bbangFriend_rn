0e3425011a9681e83eb18fe0ccf8a82876eedef8
hyuni106 <suhyuni106@hanmail.net>
2024-12-30 14:38:36 +0900

    add tag, unit, recipe, ingredient, checklist models

--- src/models/index.ts
+++ src/models/index.ts
@@ -4,3 +4,39 @@ export enum RecipeFilterType {
   COMFITS = 'comfits',
   BREAD = 'bread',
 }
+
+export interface Tag {
+  id: number;
+  key: string; // i18n key
+}
+
+export interface Unit {
+  id: number;
+  key: string; // i18n key
+}
+
+export interface Ingredient {
+  id: number;
+  name: string;
+  amount: number;
+  unit: Unit;
+}
+
+export interface Recipe {
+  id: number;
+  name: string;
+  tag: Tag;
+  desc: string;
+  temperature: number;
+  time: number;
+  ingredients: Ingredient[];
+  recipe: string[];
+  isFavorite: boolean;
+  sourceUrl?: string;
+}
+
+export interface CheckList {
+  id: number;
+  content: string;
+  isChecked: boolean;
+}
