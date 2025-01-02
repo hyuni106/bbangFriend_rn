f92392f2c53db99f9f4bbde68ae6862386e613c3
hyuni106 <suhyuni106@hanmail.net>
2025-01-02 17:33:48 +0900

    modify actions and queries in databases/recipeTags
    - 명확한 역할 분리를 위해 수정

--- src/databases/recipeTags/actions.ts
+++ src/databases/recipeTags/actions.ts
@@ -1,8 +1,17 @@
+import SQLite from 'react-native-sqlite-storage';
+import { executeSql } from 'databases';
 import { RecipeTag } from 'models';
-import { RecipeTagQuery } from './queries';
+import { RecipeTagQueries } from './queries';
 
 const fetchAllRecipeTags = async (): Promise<RecipeTag[]> => {
-  const tags = await RecipeTagQuery.getAllRecipeTags();
+  const query = RecipeTagQueries.GET_ALL;
+  const result = (await executeSql(query)) as SQLite.ResultSet;
+  const tags: RecipeTag[] = [];
+
+  for (let i = 0; i < result.rows.length; i++) {
+    tags.push(result.rows.item(i) as RecipeTag);
+  }
+
   return tags;
 };
 
--- src/databases/recipeTags/queries.ts
+++ src/databases/recipeTags/queries.ts
@@ -1,17 +1,3 @@
-import SQLite from 'react-native-sqlite-storage';
-import { executeSql } from 'databases';
-import { RecipeTag } from 'models';
-
-const getAllRecipeTags = async (): Promise<RecipeTag[]> => {
-  const query = 'SELECT * FROM RecipeTags ORDER BY id ASC;';
-  const result = (await executeSql(query)) as SQLite.ResultSet;
-  const tags: RecipeTag[] = [];
-
-  for (let i = 0; i < result.rows.length; i++) {
-    tags.push(result.rows.item(i) as RecipeTag);
-  }
-
-  return tags;
+export const RecipeTagQueries = {
+  GET_ALL: 'SELECT * FROM RecipeTags ORDER BY id ASC;',
 };
-
-export const RecipeTagQuery = { getAllRecipeTags };
