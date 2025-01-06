1be59df96a99003460a349475bde4ee920518750
hyuni106 <suhyuni106@hanmail.net>
2025-01-06 22:15:46 +0900

    refactor IngredientListItem for simplify structure
    - IngredientRow 사용 시 복잡도가 높아져 개선

--- src/components/Recipe/Editor/IngredientList/IngredientListItem.tsx
+++ src/components/Recipe/Editor/IngredientList/IngredientListItem.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { View, StyleSheet, StyleProp, ViewStyle } from 'react-native';
+import { View, StyleSheet, StyleProp, ViewStyle, TextInput } from 'react-native';
+import { useTranslation } from 'react-i18next';
 
-import { Colors } from 'styles';
-import IngredientRow from 'components/common/IngredientRow';
+import { Colors, Typography } from 'styles';
 import { AddButton, RemoveButton } from 'components/common/Button';
+import { Text, TouchableOpacity } from 'components/common/Base';
 
 interface IngredientListItemProps {
   style?: StyleProp<ViewStyle>;
@@ -15,18 +16,29 @@ interface IngredientListItemProps {
 }
 
 const IngredientListItem = (props: IngredientListItemProps): React.ReactElement => {
+  const { t } = useTranslation();
+
   const { style, isAddItem = false, name, amount, lastValue, onButtonPress } = props;
 
   return (
     <View style={[styles.root, style]}>
-      <IngredientRow
-        isEditable={true}
-        isButton={true}
-        name={name}
-        amount={amount}
-        lastValue={lastValue}
-        onButtonPress={onButtonPress}
-      />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.nameInput}
+          value={name}
+          placeholder={t('recipe.form.ingredient.fields.name')}
+          placeholderTextColor={Colors.gray4}
+        />
+        <TextInput
+          style={styles.amountInput}
+          value={amount}
+          placeholder="0"
+          placeholderTextColor={Colors.gray4}
+        />
+        <TouchableOpacity style={styles.ingredientTextWrapper} onPress={onButtonPress}>
+          <Text style={styles.ingredientText}>{lastValue}</Text>
+        </TouchableOpacity>
+      </View>
       {isAddItem ? <AddButton color={Colors.red1} /> : <RemoveButton color={Colors.red1} />}
     </View>
   );
@@ -37,6 +49,33 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     gap: 8,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 42,
+    backgroundColor: Colors.white,
+    paddingHorizontal: 16,
+    borderRadius: 8,
+  },
+  nameInput: {
+    ...Typography.body_14M,
+    color: Colors.gray1,
+    flex: 2,
+  },
+  amountInput: {
+    ...Typography.body_14M,
+    flex: 1,
+    color: Colors.gray1,
+  },
+  ingredientTextWrapper: {
+    flex: 1,
+  },
+  ingredientText: {
+    ...Typography.body_14M,
+    color: Colors.gray1,
+    textAlign: 'right',
   },
 });
 
